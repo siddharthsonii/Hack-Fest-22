4b963819b28aeff4a4be3af8419c452e66364353
ishita766 <115583588+ishita766@users.noreply.github.com>
2022-10-13 13:58:37 +0530

    Update Builder.hpp

--- Views/Builder.hpp
+++ Views/Builder.hpp
@@ -31,6 +31,14 @@ public:
     virtual QChart* geEchoSignalsChart() const = 0;
 
     virtual QChart* getDiscrepencyChart() const = 0;
+    
+    
+  using ChartControllerImplPtr = std::unique_ptr< ChartController >;
+    void fillSetupData();
+
+  void getMessageBox( const QString& _text ) const noexcept;
+
+  void drawTable( Defines::Table& _table ) const;
 
 /*---------------------------------------------------------------------------*/
 
